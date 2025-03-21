cc258d3cb7f6d13fb85e1b3a3e5e2df142ce86a5
TRIPLE-A332 <abdullah.ahmad16j@gmail.com>
2025-03-21 01:14:29 -0400

    EDA & problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys 
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
    _,_,exc_tb = error_detail.exc_info()
@@ -13,5 +14,5 @@ class CustomException(Exception):
       super().__init__(error_message)
       self.error_message=error_message_detail(error_message,error_detail=error_details)
 
-    def __str__(self):
-      return self.error_message 
+   def __str__(self):
+      return self.error_message
