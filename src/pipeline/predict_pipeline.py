d06a7a0a0843a91c6ff1d9fb88e3db8d0e9c18bc
TRIPLE-A332 <abdullah.ahmad16j@gmail.com>
2025-09-12 01:09:06 -0400

    Pipeline is ready

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,50 @@
+import sys
+from dataclasses import dataclass
+
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            model_path = 'artifact\model.pkl'
+            preprocessor_path = 'artifact\preprocessor.pkl'
+            model = load_object(file_path = model_path)
+            preprocessor = load_object(file_path = preprocessor_path)
+            data_scaled = preprocessor.transform(features)
+            prediction = model.predict(data_scaled)
+            return prediction
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+@dataclass
+class CustomData:
+    gender:str
+    race_ethnicity:str
+    parental_level_of_education: str
+    lunch: str
+    test_preparation_course:str
+    reading_score: int
+    writing_score:int
+
+    def get_data_as_DataFrame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            raise CustomException(e, sys)
