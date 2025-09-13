0ffe8d7cf0a891d1bb05ad4a73144726a9077d44
TRIPLE-A332 <abdullah.ahmad16j@gmail.com>
2025-09-13 15:14:31 -0400

    deploy config

--- .platform/hooks/prebuild/00_sys_pkgs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-if command -v dnf >/dev/null 2>&1; then
-  dnf install -y gcc gcc-c++ make
-elif command -v yum >/dev/null 2>&1; then
-  yum install -y gcc gcc-c++ make
-fi
--- .platform/hooks/prebuild/01_pip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install --no-cache-dir -r /var/app/staging/requirements.txt
